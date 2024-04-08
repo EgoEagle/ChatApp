e7c223715cbed59b99a4082473f8b5b21a64a6e8
Tony <TonyLin3591@gmail.com>
2024-04-07 22:14:44 -0400

    dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
-const express = require('express');
+import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
+
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req,res) => {
     // root route http://localhost:5000/
     res.send("Ready");
     
 });
-
-app.listen(5000,() => console.log('Running on 5000'));
+console.log(PORT);
+app.listen(PORT,() => console.log(`Running on ${PORT}`));
